e2eb53fd11a922e3176f0569eab76f56970c5cfc
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-05-10 21:15:32 -0300

    Configurando a conexão com o banco de dados mongo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
+
+/**
+ * Database setup
+ */
+
+mongoose.connect('mongodb://192.168.99.100:27017/upload', {
+  useNewUrlParser: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //lidar com requisições no padrão urlencoded //facilita envio de arquivos
